0fc693c33e3783dfc0dce140a977c145dbd90450
pratik more <pratikxmore@gmail.com>
2019-02-26 20:33:52 +0530

    Get MAX & MIN from user input

---
+++ src/assignments/loop_counting.py
@@ -0,0 +1,24 @@
+largest = None
+smallest = None
+num_list = list()
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+    try:
+        num = int(num)
+        num_list.append(num)
+        if largest is None:
+            largest = num
+        elif largest < num:
+            largest = num
+        if smallest is None:
+            smallest = num
+        elif smallest > num:
+            smallest = num
+    except:
+        print('Invalid input')
+        continue
+
+print("Maximum is ", largest)
+print("Minimum is ", smallest)
